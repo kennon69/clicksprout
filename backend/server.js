780186f0ac496254e1b60b033e5d932e25824ec6
Kennon <103473526+kennon69@users.noreply.github.com>
2025-06-02 00:58:23 -0400

    Delete backend directory

--- backend/server.js
+++
@@ -1,1 +0,0 @@
-const express = require('express'); const app = express(); app.use(express.json()); app.get('/', (req, res) => res.send('ClickSprout API')); app.listen(3000, () => console.log('Server running'));
