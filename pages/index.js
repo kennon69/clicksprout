6e6f293f3e59cabecd3e14687c80f1a8c7e97409
Kennon <103473526+kennon69@users.noreply.github.com>
2025-06-03 22:37:50 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to ClickSprout v1.0</h1>
-      <p>Submit your link to generate traffic & backlinks!</p>
-    </div>
-  );
-}
